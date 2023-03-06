82adf3bca7c6fee6a71585b9c86b67553e474d6d
khuong30072001 <64116173+khuong30072001@users.noreply.github.com>
2023-03-07 00:00:55 +0700

    init

---
+++ app.js
@@ -0,0 +1,48 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+import signatureRoute from "./routes/signature.route.js";
+import express from 'express';
+import morgan from 'morgan';
+import asyncError from 'express-async-errors';
+import cors from 'cors'
+import swaggerUI from 'swagger-ui-express';
+import swaggerJsDoc from 'swagger-jsdoc';
+import {swaggerConfigOptions} from './utils/swagger.js';
+
+
+const app = express();
+
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(cors())
+
+const specs = swaggerJsDoc(swaggerConfigOptions);
+app.use(
+    "/api-docs", swaggerUI.serve, swaggerUI.setup(specs)
+);
+
+app.use('/api/signature', signatureRoute );
+
+
+app.get('/err', function (req, res) {
+    throw new Error('Error!');
+});
+
+app.use(function (req, res) {
+    res.status(404).json({
+        error: 'Endpoint not found!'
+    });
+});
+
+app.use(function (err, req, res, next) {
+    console.log(err.stack);
+    res.status(500).json({
+        error: 'Something wrong!'
+    });
+});
+
+const PORT = process.env.app_port;
+app.listen(PORT, function () {
+    console.log(`SignatureRoute API is listening at http://localhost:${PORT}`);
+});
+
