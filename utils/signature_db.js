78b97e3e75fc48b88e4b785dc7c7abb72a3ff26e
khuong30072001 <64116173+khuong30072001@users.noreply.github.com>
2023-03-13 12:22:58 +0700

    update db connect

--- utils/signature_db.js
+++ utils/signature_db.js
@@ -1,7 +1,7 @@
 //lets require/import the mongodb native drivers.
 import mongodb from 'mongodb'
 var MongoClient = mongodb.MongoClient;
-var url = "mongodb+srv://dacnpmgroup07:123456zZ@cluster0.wynlhhc.mongodb.net/?retryWrites=true&w=majority"
+var url = "mongodb+srv://dacnpmgroup07:yDRXAJ5YlP2x5a6m@cluster0.rzo5ngl.mongodb.net/?retryWrites=true&w=majority"
 
 const mongo = await MongoClient.connect(url);
 // mongo.db("Data-Analytics").collection("KawaiiMansion_Earnings").deleteMany({})
