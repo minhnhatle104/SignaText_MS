8ae879d9a9d678304334a50567a17aa657147d6a
khuong30072001 <64116173+khuong30072001@users.noreply.github.com>
2023-03-15 22:13:02 +0700

    update port

--- app.js
+++ app.js
@@ -47,8 +47,8 @@ app.use(function (err, req, res, next) {
     });
 });
 
-const port = process.env.PORT || 6060
-app.listen(port, function () {
+const PORT = process.env.app_port;
+app.listen(PORT, function () {
     console.log(`Document API is listening at http://localhost:${PORT}`);
 });
 
