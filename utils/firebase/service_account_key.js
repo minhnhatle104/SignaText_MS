5c5203df90290e5eb5f692464a57d55f9e8b7994
khuong30072001 <64116173+khuong30072001@users.noreply.github.com>
2023-03-29 10:43:51 +0700

    update key

--- utils/firebase/service_account_key.js
+++ utils/firebase/service_account_key.js
@@ -1,16 +1,15 @@
 const serviceAccount = {
-  type: 'service_account',
-  project_id: 'signatext',
-  private_key_id: 'ae7870d3d12bdbe40c6c92a6f4ae1adf1b99fdbc',
-  private_key:
-    '-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCrJsaNkIMhlgnR\nFrgE2ZMKQwmQq0kT3RJ7CshyLiKWG0B/j6TMKsWu+6csisUUfhsK6r2P0BXNPCnG\nBe43IVTF2DuwvcT4q41dmNysWhkG2ahVhwV8kNIAU8Ra5ZlzUBoYmWK7aoTgP+78\nxXJ7p2UYXBHqZTwOg4W1jxyip8lJdDu5ZdTWBo8LWunteF50Z+SIfaLLd0f6NHye\nhte7dD7kDx2gxeIWhYi7vuwnRaHwi4ntT2iScxbYOS95LOGW80vjvX2q2ctCbC22\nUaCtr26+ktnpXyWedVriQDGOHeQtVwd8rjjlU3P7NV1+yw59ws+sOZ/pCvBF7Tyc\n9GeTLm95AgMBAAECggEAEGImWI/QVas4pli/5cr0DePr6vrShIHBjqCyHmf3LJpQ\nOsXD1z1y7fSuBJAGHJeTvCBLZBRduec78nZH3uQkasnvsNQBonI9louW+DKpuaOl\nPwatSkbMq3ZZUgAvh7/Pt7IbJuij7pmAJZc7BcY44bOSjXcCYDtL1dzst09Lb+n1\nfyqaUQ2zxiWhEW+e9t+iG4GDcsj+DNS09kEG76UjYwl5ejEjWirJoEkGKieM6CXs\neSS62M83+sjNrGfIurEsH0Iz+hXJ6y7lTAIBGgHpOfauAt8yQ37aNujDuDjf48bB\n5HEl4c6vh1KQEyRJh9WVtAymuwHIfEZ0XwLyqHETlwKBgQDxiwNWCH9SbX8ZuYd3\n0uOCnJ++0wvK6uP4yN+QKUCfkbamzSFD9T5KH+i7e6YKYB7RD8Z9Nm/aPU49VGf+\nnlCNE4XL8wi+g75pVjMkwAyGOu5pJTECWJ7NCoI0vDfmXJcdD+SX4mNuJNHIAu5w\nQVtfiBIdce/qxiEvh2diBWR/KwKBgQC1ZTRNkDczpTUw+RcZlAGT7JNw18bsH0Jn\nhDQ3uysc5Y+X343I3Mddfw/aElMjcONcvebGXd3d1O7iq18MXtb8QB3Fvgdc+MEM\ni7qgPuG4bcYbYGEZgx/03l/CjHZR2bNhl6c8d0Y7vhr5CyOnwmNA+9fnrfLzIkVX\nuITvYUeZ6wKBgHZxrZQdnJnF+eKaeNSIRGpC9Y60nP7HSj0v+3kM0ZSt9nw68MoL\nU50zooN1Mcm6ssHf03yJGjZRg7FNV6Wi6msTnLOTHMGXhuLOk0u9JScZdNl4lFzL\nKivvsEO93EeJNczZg0X7jLhxM7G5xkSjt/PTnNZ1jal5uMI+yrJlZI97AoGAcsgt\nhn8hTx7VP73+7gKH1RC4Y4lGqJdHL7WqTTG0oD5q1JFzaIy74t/KKhW6YgzG8iKA\nazlucTaSSwBhzrQcmdYAgOY0RpJppgSs/UWNE2TzHNOp3bjpectVEQhMvpJhPfn2\nqv5WhOtQqhJwZ3FL2Q0Dm+ioeU1QT6fFBqyp4C0CgYEA7xEDvx/CuLQPVlUiHj7V\nAPhEmv3IE6ElXc12Kx5FzGGI1KfaJXGysRmK+OZPpWJaBE1cTNa5laff9XhM+ZCp\nRq/Z9qJtgLAuENywTXM3ugnnPgWJ1Qlv20TT4o73hXOzbezdMyb1o/tXWG+IE91w\ni5efJnfVLQGYzXL8CQ8uuTI=\n-----END PRIVATE KEY-----\n',
-  client_email: 'firebase-adminsdk-xg2a7@signatext.iam.gserviceaccount.com',
-  client_id: '112166862529747134224',
-  auth_uri: 'https://accounts.google.com/o/oauth2/auth',
-  token_uri: 'https://oauth2.googleapis.com/token',
-  auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
-  client_x509_cert_url:
-    'https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-xg2a7%40signatext.iam.gserviceaccount.com',
+ "type": "service_account",
+ "project_id": "signatext-v02",
+ "private_key_id": "05ad80ea7d9f636883cd118b280a15d3b7b1d640",
+ "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCX3j4lNHJGDoi1\niPyZTThZUHLLXAZgsLHEoZ9wbVbaGlR1aCxBG18rxZaYzQPzEDSHUupL+5WyyY7t\nf6p7cDc9gBmRC7yWkD7mlBczUXqwuu5gK4fcHFHK4Mtmkm8RSlnssydwKuL9SbFI\n4rSgajaCR7ZuHT+reJC85eXL/yQMTyUz6hI8SSlu0VlgzRM43tBbRfDwDTaqKcM7\niV7pdVZzoKVI2Xmui4WquiatJ8yi7P5I5bF38l06uuhq+bnFZp/OE8tsR7BN4TDS\nE0x0pWGJr/+au5/Vts0mBUX2/vzdDIK+TMVs2FNcFhUsudLhFTS9BHT7goEawRhg\nLA1JFbhJAgMBAAECggEADtevjPEQ5JAt1Aku/9gcVF1QlNe5/1fxjdFKfQFQqlkF\nrWJYpeXUEbn6T5WE5+vq6FlmEPZGEY7Rr1xJeA419eanyQVjwXbYUO6DDHbPhgeE\n/eTvPqfNZiW9d1lDLeZNftzdE0E7xUu/HgubIezzhhzWY3FsHDj4s1+IK510Y1fv\nwnf5nc+WL6imuNFUHa5dl4UYwADEEdXVfwCWZuGyY3JmlybCG9E1udCGKMwQTPZN\n4G3PnXYv8OF6rwKByo026k6aSwBWBL+c1Z6Qlpezb59t0XtvKivSzAOL5FBpH0jb\nsHD6zKBglROR0VkRc13UYLIicE9t6iN5oCkSFbQrcQKBgQDRqY/CQLgLR/YYO1Xt\ndAc1nG0zh3jWkhWKzx5hWYKnsbsxtLw0eRMxP69MPJTFB7IolwKEZ/Us8euEBvnt\nlbNUIWPU3JqkFa8uCjLbaV/12TtJzWOUn8mRfgq9m0UCsGOt58pqdwlB5vz4iWgv\nRtzHAc36krQ6jahoHyIboN9GOQKBgQC5bsFoDpVhJff7Mte6VF419K5VQgeimgx7\nvHSbSs80i2X7aGAbPpR7AZoR09nVh8qLzgHjwVhLZ6xxS7S+4773s3D0X7MAjjOE\nLtA2Fb8IOdntLSRhGQAVjuUeWpC3w8tUbwuYhYqssy/yo9AlVMZa73ILilbVDehf\nIjNgQKaCkQKBgGqs7GE4zqumvJcNsaW0kGp9oa5DMn9yKpXK1jlP8sNiEc2zmMLW\nC6bWMVoHxOYPCV3Pn89AmaV5nYw6hHWW4nxe1M2cRO5RviUduUyb6+BJ2/msEAux\n1Tw6tjpMwysgFkeIlRvgXwxx+6p1N1Y+ptx2lZf1hs/3/YSkupT2IEQ5AoGARaWC\nwPmRQ57GKDQtcI+VnwBapi0ZvwnLHIJo7ZbIgnzE/gty5Wi4zgaI/p8qVxgcjCwA\n67FH33J+A0OjDHtv04szJqX3DoNaeBA5Jcc/K/DGmF13J30DWhWEmsT7S/ofGYRn\nv46QylK29JRwbkUMB0Wd+RWpjXYey/SFhWZi4/ECgYAzDdDGDUGsCiH5KRwLI1fo\niWsk2Y52caDHhw6TWnZWTah0gV9O6u9WIMDN0m6MNrhhq4JzxkneMhgJjrqRUf3P\nLGmdCLj9BxlzQzERPbuzA6lJn2yfes5ANuDG6rmj+XkVxt9/Oif4Nj61aYUTOCOE\nuyq+5TqINasbklDH4lYatg==\n-----END PRIVATE KEY-----\n",
+ "client_email": "firebase-adminsdk-tvtbh@signatext-v02.iam.gserviceaccount.com",
+ "client_id": "111066152247248824639",
+ "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+ "token_uri": "https://oauth2.googleapis.com/token",
+ "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+ "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-tvtbh%40signatext-v02.iam.gserviceaccount.com"
 }
+
 
 export default serviceAccount
