124a4e6749f4cf57f2d1b3467525226c210ac512
khuong30072001 <64116173+khuong30072001@users.noreply.github.com>
2023-04-05 15:02:34 +0700

    update save docslist

--- routes/notification.route.js
+++ routes/notification.route.js
@@ -1,5 +1,7 @@
 import express from "express";
 import sendEmail from '../utils/email.js';
+import firebase from '../utils/firebase/firebase_init.js'
+
 
 const router = express.Router()
 
@@ -11,18 +13,85 @@ router.get("/test", async(req,res)=>{
 });
 
 router.post('/forward', async (req, res) => {
-    const email = req.body.email;
-    const name = req.body.name;
-    const userId = req.body.user_id;
-    const documentId = req.body.document_id;
+    const data = req.body;
+    // const name = req.body.name;
+    // const userId = req.body.user_id;
+    // const documentId = req.body.document_id;
+    const recipientList = data.state.recipientList
+    const namefile = data.state.fileNamePdf
+    const senderUI = data.userID
 
-    const VERIFY_EMAIL_SUBJECT = 'SignaText: Sign a document';
-    const MESSAGE = `
-            Dear ${name},\n
-            Nguyen Vu Duy Khuong sent you a document to review and sign\n
-            Please access this link to sign the document: https://fe-docusign.vercel.app/document/other/signPDF`;
+    ////// get info + send email
+    const usersCollection = firebase.firestore().collection('users')
+    const senderName = await usersCollection.where('userId', '==', senderUI)
+        .get()
+        .then((querySnapshot) => {
+            if (querySnapshot.empty) {
+                console.log('No user found with id: ', senderUI);
+            } else {
+                // There should only be one user with the given email
+                const userDoc = querySnapshot.docs[0];
+                const name = userDoc.data().full_name;
+                return name
+            }
+        })
+        .catch((error) => {
+            console.error('Error getting user with email', email, ':', error);
+        });
 
-    sendEmail(email, VERIFY_EMAIL_SUBJECT, MESSAGE);
+    const recieverName = []
+    const permission = []
+    const userReceiveID = []
+    for (const c of recipientList){
+        await usersCollection.where('email', '==', c.email)
+            .get()
+            .then((querySnapshot) => {
+                if (querySnapshot.empty) {
+                    console.log('No user found with email:', c.email);
+                } else {
+                    // There should only be one user with the given email
+                    const userDoc = querySnapshot.docs[0];
+                    const fullname = userDoc.data().full_name;
+                    const receiverID = userDoc.data().userId;
+                    recieverName.push(fullname)
+                    permission.push(c.permission)
+                    userReceiveID.push(receiverID)
+
+                    const VERIFY_EMAIL_SUBJECT = `SignaText: ${c.permission} a document`;
+                    const per = c.Permission == "Needs to view"?"view":"sign"
+                    const MESSAGE = `
+                        Dear ${fullname},\n
+                        ${senderName} sent you a document to ${per}!\n
+                        Please access this link to sign the document: https://fe-docusign.vercel.app/document/other/signPDF`;
+                    sendEmail(c.email, VERIFY_EMAIL_SUBJECT, MESSAGE);
+                }
+            })
+            .catch((error) => {
+                console.error('Error getting user with email', c.email, ':', error);
+            });
+    }
+
+    /// add to doclist
+    const docList = {
+        date: new Date(),
+        namefile,
+        senderName,
+        receiverName: recieverName,
+        permission,
+        userCreateID: senderUI,
+        userReceiveID
+    }
+    console.log(docList)
+    const docListCollection = firebase.firestore().collection('docslist')
+
+    docListCollection
+        .add(docList)
+        .then((docRef) => {
+            console.log('New user document added with ID:', docRef.id);
+        })
+        .catch((error) => {
+            console.error('Error adding new user document:', error);
+        });
 
     return res.json({
         status: true,
