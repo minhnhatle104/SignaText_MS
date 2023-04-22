0a45fd762021a824c660d31e630b4ee5cdac1b17
minhnhatle104 <lnminh104@gmail.com>
2023-04-23 03:20:16 +0700

    deploy buffer key firebase

--- BE_Authentication_Service/utils/serviceAccount.js
+++ BE_Authentication_Service/utils/serviceAccount.js
@@ -7,7 +7,7 @@ const serviceAccount = admin.initializeApp({
     "type": process.env.TYPE,
     "project_id": process.env.PROJECT_ID,
     "private_key_id": process.env.PRIVATE_KEY_ID,
-    "private_key": process.env.PRIVATE_KEY,
+    "private_key": Buffer.from(process.env.PRIVATE_KEY).toString(),
     "client_email": process.env.CLIENT_EMAIL,
     "client_id": process.env.CLIENT_ID,
     "auth_uri": process.env.AUTH_URI,
