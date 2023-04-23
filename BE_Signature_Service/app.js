896fca7d38e81abbc20ef29ac0cd0d525281d6ed
nntt1210 <58521730+nntt1210@users.noreply.github.com>
2023-04-23 11:50:32 +0700

    Remove unused files

--- BE_Signature_Service/app.js
+++ BE_Signature_Service/app.js
@@ -1,58 +1,61 @@
-import * as dotenv from 'dotenv'
-dotenv.config()
-import signatureRoute from './routes/signature.route.js'
 import express from 'express'
 import morgan from 'morgan'
 import cors from 'cors'
-import swaggerUI from 'swagger-ui-express'
-import swaggerJsDoc from 'swagger-jsdoc'
-import { swaggerConfigOptions } from './utils/swagger.js'
-import Middleware from './middlewares/auth.js'
-import multer from 'multer'
+import { ApolloServer } from 'apollo-server-express'
+import { GraphQLError } from 'graphql'
+import typeDefs from './typeDefs.js'
+import resolvers from './resolvers.js'
+import firebase from './utils/firebase/firebase_init.js'
+import graphqlUploadExpress from 'graphql-upload/graphqlUploadExpress.js'
+
 const app = express()
+
+const server = new ApolloServer({
+  uploads: false,
+  typeDefs,
+  resolvers: resolvers,
+  playground: true,
+  context: async ({ req }) => {
+    const token = req.headers.authorization
+
+    try {
+      if (!token) {
+        throw new GraphQLError('Unauthorized', {
+          extensions: {
+            code: 'UNAUTHORIZED',
+            http: { status: 401 },
+          },
+        })
+      }
+
+      const decodeValue = await firebase.auth().verifyIdToken(token)
+
+      if (decodeValue) {
+        return { token, userId: decodeValue.user_id }
+      }
+      throw new GraphQLError('Unauthorized', {
+        extensions: {
+          code: 'UNAUTHORIZED',
+          http: { status: 401 },
+        },
+      })
+    } catch (error) {
+      console.log('error', error)
+      return {
+        status: 500,
+        message: 'Internal server error',
+      }
+    }
+  },
+})
 
 app.use(express.json())
 app.use(morgan('dev'))
 app.use(cors())
 
-console.log('Signature key: ', process.env.PRIVATE_KEY)
+app.use(graphqlUploadExpress({ maxFileSize: 5000000, maxFiles: 1 }))
 
-app.use(Middleware.decodeToken)
-
-const specs = swaggerJsDoc(swaggerConfigOptions)
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(specs))
-
-app.use('/api/signature', signatureRoute)
-
-app.get('/err', function (req, res) {
-  throw new Error('Error!')
-})
-
-app.use(function (req, res) {
-  res.status(404).json({
-    success: false,
-    message: 'Endpoint not found!',
-    result: {},
-  })
-})
-
-app.use(function (err, req, res, next) {
-  console.log(err)
-  if (err instanceof multer.MulterError) {
-    res.status(400).json({
-      success: false,
-      message: err.message || err.code,
-      result: {},
-    })
-  } else {
-    res.status(500).json({
-      success: false,
-      message: 'Internal Server Error',
-      result: {},
-    })
-  }
-})
-
+server.applyMiddleware({ app })
 const PORT = process.env.app_port || 3000
 app.listen(PORT, function () {
   console.log(`SignatureRoute API is listening at http://localhost:${PORT}`)
