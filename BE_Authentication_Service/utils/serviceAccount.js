6873863dddf2e0b13d522dfde1d3e681ff4eca77
minhnhatle104 <lnminh104@gmail.com>
2023-04-24 10:42:22 +0700

    Update private key

--- BE_Authentication_Service/utils/serviceAccount.js
+++ BE_Authentication_Service/utils/serviceAccount.js
@@ -2,13 +2,19 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 import admin from 'firebase-admin'
 
+const {privateKey} = JSON.parse(process.env.PRIVATE_KEY)
+
+console.log(process.env.PRIVATE_KEY)
+console.log("privateKey:",privateKey)
+console.log(typeof process.env.PRIVATE_KEY)
+
 
 const serviceAccount = admin.initializeApp({
   credential: admin.credential.cert({
     "type": process.env.TYPE,
     "project_id": process.env.PROJECT_ID,
     "private_key_id": process.env.PRIVATE_KEY_ID,
-    "private_key": process.env.PRIVATE_KEY ? process.env.PRIVATE_KEY.replace(/\\n/g, '\n') : undefined,
+    "private_key": privateKey,
     "client_email": process.env.CLIENT_EMAIL,
     "client_id": process.env.CLIENT_ID,
     "auth_uri": process.env.AUTH_URI,
@@ -19,9 +25,5 @@ const serviceAccount = admin.initializeApp({
   ),
   storageBucket: "signatext-v02.appspot.com"
 });
-
-console.log(process.env.PRIVATE_KEY)
-console.log("replace: ",process.env.PRIVATE_KEY.replace(/\\n/g, '\n'))
-console.log(typeof process.env.PRIVATE_KEY)
 
 export default serviceAccount
