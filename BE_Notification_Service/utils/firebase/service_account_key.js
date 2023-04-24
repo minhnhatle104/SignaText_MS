799f49f1426c6e658d89679e838dcee83263fce6
minhnhatle104 <lnminh104@gmail.com>
2023-04-24 19:35:55 +0700

    Integrate jest in authen and CI

--- BE_Notification_Service/utils/firebase/service_account_key.js
+++ BE_Notification_Service/utils/firebase/service_account_key.js
@@ -2,16 +2,16 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 const serviceAccount = {
-  "type": process.env.TYPE,
-  "project_id": process.env.PROJECT_ID,
-  "private_key_id": process.env.PRIVATE_KEY_ID,
-  "private_key": process.env.PRIVATE_KEY,
-  "client_email": process.env.CLIENT_EMAIL,
-  "client_id": process.env.CLIENT_ID,
-  "auth_uri": process.env.AUTH_URI,
-  "token_uri": process.env.TOKEN_URI,
-  "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER_x509_CERT_URL,
-  "client_x509_cert_url": process.env.CLIENT_X509_CERT_URL
+  "type": "service_account",
+  "project_id": "signatext-v02",
+  "private_key_id": "f495e60dc3d0f0bcd3f679ce93fc7a368a3a8032",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDR9q3cszxBB43y\naLkBDZQKpg2ceBuud6Uz0M1utgYpIOYDHvZ4qKhoetIwO2sL64lqxIT4xER3LZrS\nm4I2fcWdnJ6bhAcfdvB7q8OSAgkR+zbEMGFPuT9zkvIjF6iDr29U/7W6tCGZn+sn\nHFr7P4GAcgA3o5itBvu+fwOTP4z969rzaUClUQmUaYbViNedpkVj0iiG73RRhd7C\nHkCUytuwr3z29IU6UTdjNnqAsmA9O5v5QyUWb2nq/DYuFUmmtRAZp0BKO7grbp8y\nRI0Yv20kou1b+Pa7s16HDxT1hW1NWneK1N22cOoljTmoMYVktJMbnYtsOXe/3zpv\nPaE077evAgMBAAECggEACxBqMgaZ5r1crAj7ssQB6Ju+fN/RaZyxbm0S3kImSfWL\nWV/A0jPlB/hs6hC/f9XOHHu1nBxPlU4oae+66V8wUjFmJEDNVUmhu4qeGCwWSUXJ\nKYZ5IkfbQZ5dNx8wiJvujHabH3cNjDhinmF+o93BV8DqJQRwb7G+A1hTGmfPaLP0\nFcEP2rAiBLUmjumApfLA6eYIBJjG4TDOUivk5gFcZz3g5tgQW/9jCqnt9GIf7GB3\nCRT3fqNtEmvsm2VDK4CXC0ZiPTiVMYRRk/4jNQC1wCtlH5/0Ca8SWoxxYVOjft7w\nrwhsxdVW/aq9TDu0TsLCkLQvdzvm/fwlO7k5N0oglQKBgQDxklEYn0ieX33G6p/7\n82+MgSZHB6RsXrWrQhqqtAtNgR+xY/hrUGwbdlLfsemh9daj3FjGgOt3oIk1j5+f\nB0m6UxbAGZe329uSHpNlXx0CH8SxqsWxq+ayuzBs/gZvk7pYaFTK40qzeUOcfUod\nxuCYNIpgv+ftl86odY6mbZpWjQKBgQDegRHPdaIR+AMw/qj+GdH+Lv1tcNp4SpqR\npNbrGFyjhuZ6F2EfSImrbszNOnxed/5PD678zUPr7VhZ9MqAM7rCa3eExzUJ1DAe\n4a1JJXWWzOQRSig2csn2bx0wKAxdpUg9O4wiOWZr0nI9ECTHilPTgaZML0/a49Hq\nh9OaaWNmKwKBgG8W9Eg4Z7K9AyBIoU3CAxhABBRCBXD4lN6uxYiex1va93IiIQlo\nsG5omW/ZZzFd1lkAaarPenlG0BJbe/Ir7dHLh/jLTCP27SBjAzFo7fQeG9wARm2Q\nOcUTpA9bOiZxbw5c3DF5GM80As2iKEVNQdd8we4AEm4Gk7iQZwtN9925AoGBANN4\nWkQRahKn4+iZ8GVohuONJ4HvY0Q2q4dG4piYE9u7p6tcMwt3dpi/etiHAJMMemAL\n6OUN9cLyVilQ9XE5OQP0/aftaORJjd6b4IUHnCXR3+Tq1SNCn96epmARgAc0wl6o\nDLyuT0Ckx7xvbnVTa+42Z6Y6xSuloOnFZAB6r2T3AoGBANJhXO5kKk+ULz0DZYMt\nGvQ7i911P4lXlep5SQQMj338HL8f9C5iONj15W8qL4teaqH66RllIG4hBt+vZvDW\n5EnZuOkjHXylVes9dIXT4KJohkrJ3+ko3qhFxn7iwu4H1fY07b7s/yG9QI8gwW7D\nmfDXmS23aEXCOer0BrifcXGk\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk-tvtbh@signatext-v02.iam.gserviceaccount.com",
+  "client_id": "111066152247248824639",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-tvtbh%40signatext-v02.iam.gserviceaccount.com"
 }
 
 export default serviceAccount
