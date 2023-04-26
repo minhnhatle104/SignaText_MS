206681b17902ba05accb856017fe1215ef6032e1
minhnhatle104 <lnminh104@gmail.com>
2023-04-27 03:51:46 +0700

    update_code

--- BE_Notification_Service/routes/notification.route.js
+++ BE_Notification_Service/routes/notification.route.js
@@ -9,7 +9,7 @@ const router = express.Router()
 //router.get("/transactions", authUser, authorization(role.ADMIN), async(req,res)=>{
 router.get("/test", async(req,res)=>{
     return res.status(200).json({
-        message: "success hehe"
+        message: "success"
     })
 });
 
